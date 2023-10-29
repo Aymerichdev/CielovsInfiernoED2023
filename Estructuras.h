611822fb5a44abcaf613e5e775546353a5e171d4
Victor Aymerich Quesada <vaymerich@estudiantec.cr>
2023-10-29 12:46:31 -0600

    Inicio

---
+++ Estructuras.h
@@ -0,0 +1,8 @@
+//Includes de las librerias necesarias para el programa
+#include <iostream>
+
+//Funciones
+
+//Structs
+
+//Variables globales
---
+++ main.cpp
@@ -0,0 +1,6 @@
+#include "Estructuras.h"
+
+int main() {
+    std::cout << "Hello, world!" << std::endl;
+    return 0;
+}
